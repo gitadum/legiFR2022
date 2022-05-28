2a9f93fe14fceb11182d1b06de4dab17bc1e86ad
Adrien Dumont <dev@adum.io>
2022-05-28 20:17:59 +0200

    read data from table file

--- assembly_projection.py
+++ assembly_projection.py
@@ -1,30 +1,32 @@
 #! ./envs/bin/python3
 # -*-coding: utf-8-*-
 
+import pandas as pd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 from numpy import mean
 import poli_sci_kit
+import json
+ 
+df = pd.read_csv('data.csv', header=0, index_col=['Polling firm', 'Date'])
+with open('group_colors.json', 'r') as color_file:
+    colors = json.load(color_file)
 
-parl_group = ["PCF","LFI","PS","EELV","Ens.","LR","DLF","RN","Req.","Others"]
-groupColor = ['red', 'firebrick', 'salmon', 'limegreen', 'orange', 
-              'royalblue', 'blueviolet', 'navy', 'saddlebrown', 'lightgray']
+poll = df.groupby(['Polling firm','Date']).mean()
+poll = poll.apply(lambda x: round(x / poll.sum(axis=1) * 577, 0))
 
-projec_min = [9,60,18,20,295,32,0,42,0,3]
-projec_max = [16,86,34,35,345,52,1,68,3,7]
-projec_avg = []
+Pollster = "OpinionWay"
+Date = "2022-05-24"
+
+selected_poll = poll.loc[(Pollster, Date)]
+parl_group = list(df.columns)
+groupColor = list(colors.values())
+seat_alloc = [int(n) for n in selected_poll.values]
 
 try:
-    assert len(parl_group) == len(groupColor) == len(projec_min) == len(projec_max)
+    assert len(parl_group) == len(groupColor) == len(seat_alloc)
 except AssertionError:
-    print(len(parl_group), len(groupColor), len(projec_min), len(projec_max))
-
-for i in range(len(parl_group)):
-    group_avg = round(mean([projec_min[i],projec_max[i]]),0)
-    projec_avg.append(group_avg)
-
-seat_alloc = [int(round(i / sum(projec_avg) * 577, 0)) for i in projec_avg]
-#print(seat_alloc)
+    print(len(parl_group), len(groupColor), len(seat_alloc))
 
 zeroSeat = []
 for i in range(len(seat_alloc)):
@@ -34,10 +36,6 @@ for i in range(len(seat_alloc)):
 parl_group = [parl_group[i] for i in range(len(parl_group)) if i not in zeroSeat]
 groupColor = [groupColor[i] for i in range(len(groupColor)) if i not in zeroSeat]
 seat_alloc = list(filter(lambda x: x != 0, seat_alloc))
-
-#print(parl_group, len(parl_group))
-#print(groupColor, len(groupColor))
-#print(seat_alloc, len(seat_alloc))
 
 fig, ax = plt.subplots(figsize=(8,6))
 
@@ -53,7 +51,7 @@ poli_sci_kit.plot.parliament(
 
 ax.legend(["{g} : {s}".format(g=group, s=seat)
            for group, seat in zip(parl_group, seat_alloc)])
-ax.text(0,-1,'source: Harris Interactive, May 23rd, 2022', fontsize=12)
+ax.text(0,-1,'source: {p}, {d}'.format(p=Pollster, d=Date), fontsize=12)
 ax.set_title("Polls - France: 2022 Legislative elections - seat projections")
 
 fig.tight_layout()
