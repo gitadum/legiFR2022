41fe9520f7701986c4e434302046e6fdfbc4e86b
Adrien Dumont <dev@adum.io>
2022-06-18 08:29:26 +0200

    chart style is set in a separate script

---
+++ styles.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+
+font = {
+    'family': 'sans-serif',
+    'sans-serif': 'arial',
+    'weight': 'normal',
+    'size': 16
+    }
+title_font = {
+    'font': 'Yanone Kaffeesatz',
+    'fontweight': 'bold',
+    'fontsize': 24
+    }
+subtitle_font = {
+    'font': 'Yanone Kaffeesatz',
+    'fontweight': 'normal',
+    'fontsize': 20
+    }
+
+plt.rc('font', **font)
